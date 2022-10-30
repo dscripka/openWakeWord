36a5c6e9ad1b947a16fde1efd95ce2ee96fe7dbb
dscripka <david.scripka@gmail.com>
2022-10-30 17:00:43 -0400

    Fixed typos in requirements

--- setup.py
+++ setup.py
@@ -5,10 +5,28 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="openwakeword",
-    version="0.0.1",
+    version="0.1.0",
     install_requires=[
         'onnxruntime>=1.10.0,<2'
     ],
+    extras_require={
+        'test': [
+                    'pytest>=7.2.0,<8',
+                    'pytest-cov>=2.10.1,<3',
+                    'pytest-flake8>=1.1.1,<2',
+                    'pytest-mypy>=0.10.0,<1'
+                ],
+        'full': [
+                    'mutagen>=1.46.0,<2',
+                    'speechbrain>=0.5.13,<1',
+                    'pytest>=7.2.0,<8',
+                    'pytest-cov>=2.10.1,<3',
+                    'pytest-flake8>=1.1.1,<2',
+                    'pytest-mypy>=0.10.0,<1',
+                    'plotext>=5.2.7,<6',
+                    'sounddevice>=0.4.1,<1'
+                ]
+    },
     author="David Scripka",
     author_email="david.scripka@gmail.com",
     description="An open-source audio wake word (or phrase) detection framework with a focus on performance and simplicity",
