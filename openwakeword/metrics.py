69afd1e3e3571c601bc74d9866b637b4cf657ca6
dscripka <david.scripka@gmail.com>
2023-01-01 19:08:37 -0500

    Reworked method in metrics.py for false positive calculations, added example notebooks for performance calculations

--- openwakeword/metrics.py
+++ openwakeword/metrics.py
@@ -16,9 +16,35 @@
 import re
 from tqdm import tqdm
 import numpy as np
+from typing import List
 
 
 # Define metric utility functions specific to the wakeword detection use-case
+
+def get_false_positives(scores: List, threshold: float, grouping_window: int=50):
+    """
+    Counts the number of false-positives based on a list of scores and a specified threshold.
+
+    Args:
+        scores (List): A list of predicted scores, between 0 and 1
+        threshold (float): The threshold to use to determine false-positive predictions
+        grouping_window (int: The size (in number of frames) for grouping scores above
+                                 the threshold into a single false positive for counting
+
+    Returns:
+        int: The number of false positive predictions in the list of scores
+    """
+    bin_pred = np.array(scores) >= threshold
+    bin_pred_string = ''.join(["1" if i else "0" for i in bin_pred])
+    transitions = list(re.finditer("01", bin_pred_string))
+    n = grouping_window
+    for t in transitions:
+        if bin_pred[t.end()] != 0:
+            bin_pred[t.end():t.end() + n] = [0]*n
+        
+    return sum(bin_pred)
+
+
 def generate_roc_curve_fprs(
                             scores: list,
                             n_points: int = 25,
@@ -33,7 +59,8 @@ def generate_roc_curve_fprs(
     Args:
         scores (List): A list of predicted scores, between 0 and 1
         n_points (int): The number of points to use when calculating false positive rates
-        time_per_prediction: The time (in seconds) that each prediction represents
+        time_per_prediction (float): The time (in seconds) that each prediction represents
+        kwargs (dict): Any other keyword arguments to pass to the `get_false_positives` function
 
     Returns:
         list: A list of false positive rates per hour at different score threshold levels
@@ -45,11 +72,8 @@ def generate_roc_curve_fprs(
     # Calculate true positive rate
     fprs = []
     for threshold in tqdm(np.linspace(0.01, 0.99, num=n_points)):
-        # Remove repeated predictions from data to not overcount false positives
-        bin_pred = ''.join(["1" if i else "0" for i in np.array(scores) >= threshold])
-        bin_pred = re.sub("1(0){1,5}1", "1", bin_pred)
-        bin_pred = re.sub("0(1){1,50}0", "1", bin_pred)
-        fprs.append(len(re.findall('1', bin_pred))/total_hours)
+        fpr = get_false_positives(scores, threshold=threshold, **kwargs)
+        fprs.append(fpr/total_hours)
 
     return fprs
 
