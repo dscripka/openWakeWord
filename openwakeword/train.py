d1bce17e0b7cf01a1ebbf0f3b122dc020a8128ce
AntorFR <contact@antor.fr>
2024-03-12 23:18:16 +0100

    Add config option for other language

--- openwakeword/train.py
+++ openwakeword/train.py
@@ -669,7 +669,9 @@ if __name__ == '__main__':
             generate_samples(
                 text=config["target_phrase"], max_samples=config["n_samples"]-n_current_samples,
                 batch_size=config["tts_batch_size"],
-                noise_scales=[0.98], noise_scale_ws=[0.98], length_scales=[0.75, 1.0, 1.25],
+                model = config["generator_model"],
+                min_phoneme_count=config["min_phoneme_count"], 
+                noise_scales=config["noise_scales_train"], noise_scale_ws=config["noise_scales_train"], length_scales=[0.75, 1.0, 1.25],
                 output_dir=positive_train_output_dir, auto_reduce_batch_size=True,
                 file_names=[uuid.uuid4().hex + ".wav" for i in range(config["n_samples"])]
             )
@@ -685,7 +687,9 @@ if __name__ == '__main__':
         if n_current_samples <= 0.95*config["n_samples_val"]:
             generate_samples(text=config["target_phrase"], max_samples=config["n_samples_val"]-n_current_samples,
                              batch_size=config["tts_batch_size"],
-                             noise_scales=[1.0], noise_scale_ws=[1.0], length_scales=[0.75, 1.0, 1.25],
+                             model = config["generator_model"],
+                             min_phoneme_count=config["min_phoneme_count"], 
+                             noise_scales=config["noise_scales_test"], noise_scale_ws=config["noise_scales_test"], length_scales=[0.75, 1.0, 1.25],
                              output_dir=positive_test_output_dir, auto_reduce_batch_size=True)
             torch.cuda.empty_cache()
         else:
@@ -706,7 +710,11 @@ if __name__ == '__main__':
                     include_input_words=0.2))
             generate_samples(text=adversarial_texts, max_samples=config["n_samples"]-n_current_samples,
                              batch_size=config["tts_batch_size"]//7,
-                             noise_scales=[0.98], noise_scale_ws=[0.98], length_scales=[0.75, 1.0, 1.25],
+                             model = config["generator_model"],
+                             min_phoneme_count=config["min_phoneme_count"], 
+                             noise_scales=config["noise_scales_train"],
+                             noise_scale_ws=config["noise_scales_train"],
+                             length_scales=[0.75, 1.0, 1.25],
                              output_dir=negative_train_output_dir, auto_reduce_batch_size=True,
                              file_names=[uuid.uuid4().hex + ".wav" for i in range(config["n_samples"])]
                              )
@@ -729,7 +737,11 @@ if __name__ == '__main__':
                     include_input_words=0.2))
             generate_samples(text=adversarial_texts, max_samples=config["n_samples_val"]-n_current_samples,
                              batch_size=config["tts_batch_size"]//7,
-                             noise_scales=[1.0], noise_scale_ws=[1.0], length_scales=[0.75, 1.0, 1.25],
+                             model = config["generator_model"],
+                             min_phoneme_count=config["min_phoneme_count"], 
+                             noise_scales=config["noise_scales_test"],
+                             noise_scale_ws=config["noise_scales_test"],
+                             length_scales=[0.75, 1.0, 1.25],
                              output_dir=negative_test_output_dir, auto_reduce_batch_size=True)
             torch.cuda.empty_cache()
         else:
